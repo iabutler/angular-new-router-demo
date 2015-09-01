d359c185f494b1f1b60fb528df368ab259eeb54a
ian <ian.butler@talentjumpstart.com>
2015-09-01 13:20:58 -0400

    with authentecation

--- js/app.js
+++ js/app.js
@@ -4,7 +4,41 @@ app.config(['$locationProvider', function($locationProvider) {
         $locationProvider.html5Mode({
             enabled: true
         });
-    }]);
+}]);
+
+app.factory('apiCalls', function() {
+
+    return{
+        login: function(user){
+            //this is super basic and is juts a placeholder for logic you would actually use
+            console.log();
+            return user.pass === 'testPass';
+        }
+    };
+
+});
+
+app.factory('UserInfo', function() {
+    var user = {
+        email: '',
+        name: '',
+        pass: '',
+        type: ''
+    };
+
+    return{
+        update: function(newUser) {
+            user.email = newUser.email;
+            user.name = newUser.name;
+            user.pass = newUser.pass;
+            user.type = newUser.type;
+            return true;
+        },
+        getUser: function() {
+            return user;
+        }
+    };
+});
 
 app.controller('SiteCtrl',['$router', function($router){
 
@@ -31,29 +65,51 @@ app.controller('SiteCtrl',['$router', function($router){
             component: 'route3'
         }
         ]);
-    $router.default
+
+
+
+
 
     this.user = {
+        email: "test@test.com",
         name: "Johnny",
         pass: "PaSsWoRd",
-        type: "school"
+        type: '',
     };
+
 }]);
 
-app.controller('Route1Controller', function(){
-    console.log("Loaded Controller 1");
+app.controller('Route1Controller',['UserInfo', function(UserInfo){
+
+    this.user = {
+        name: '',
+        email: '',
+        pass: '',
+        type: ''
+    };
+
+    this.canDeactivate = function(){
+        return UserInfo.update(this.user);
+    };
+
     this.dataSet = {
         string: 'some data here',
         string2: 'smore more data'
     };
-});
+}]);
 
-app.controller('Route2Controller', function(){
+app.controller('Route2Controller', ['apiCalls', 'UserInfo', function(apiCalls, UserInfo){
+
+    this.canActivate = function(){
+        console.log(UserInfo.getUser());
+        return apiCalls.login(UserInfo.getUser());
+    };
+
     this.dataSet = {
         string: 'different data',
         string2: 'more different data'
     };
-});
+}]);
 
 app.controller('Route3Controller', function(){
     this.dataSet = {
